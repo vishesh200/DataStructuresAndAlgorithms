ecc674911053240e3985a95880d5e9caebe0dc3d
Vishesh <visheshrathi2008@gmail.com>
2023-03-17 19:39:00 +0530

    Finding Subsets Code added

---
+++ BackTracking/Subsets.java
@@ -0,0 +1,19 @@
+package BackTracking;
+
+public class Subsets {
+    public static void findSubsets(String str,String ans,int i) {
+        if(i == str.length()) {
+            if(ans.length() == 0) {
+                System.out.println("null");
+            }
+            System.out.println(ans);
+            return;
+        }
+        findSubsets(str,ans+str.charAt(i),i+1);
+        findSubsets(str,ans,i+1);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        findSubsets(str,"",0);
+    }
+}
