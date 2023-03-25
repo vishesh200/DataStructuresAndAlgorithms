de3f5843dced4706d6877cfd084dd38db6e4ea67
Vishesh <visheshrathi2008@gmail.com>
2023-03-25 12:39:17 +0530

    Cycle Removal Code added

--- LinkedLists/CycleDetectionAndRemoval.java
+++ LinkedLists/CycleDetectionAndRemoval.java
@@ -37,12 +37,49 @@ public class CycleDetectionAndRemoval {
         }
         return false;
     }
+
+    // Method for removing the cycle in the Linked List
+    public static void removeCycle() {
+        // Firstly checking if the cycle exists or not
+        boolean isCyclic = false;
+        Node slow = head;
+        Node fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == slow) {
+                isCyclic = true;
+                break;
+            }
+        }
+        if(isCyclic == false) {
+            return;
+        }
+        /* Initializing our slow pointer to the head and then moving the slow and fast
+        pointers everytime by 1 position to see where they meet next and mathematically
+        that happens at the point where linked list forms a cycle so the value of the
+        node just before the meeting point will be stored inside prev pointer and its
+        next will then be assigned the value of null to remove the cycle */
+        slow = head;
+        Node prev = null;
+        while(slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+        // Removing the cycle
+        prev.next = null;
+    }
     public static void main(String[] args) {
         CycleDetectionAndRemoval a = new CycleDetectionAndRemoval();
         head = new Node(1);
         head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = head;  // 1 -> 2 -> 3 -> 1
+        Node temp = new Node(3);
+        head.next.next = temp;
+        temp.next = new Node(4);
+        temp.next.next = temp;
+        System.out.println(isCyclic());
+        removeCycle();
         System.out.println(isCyclic());
     }
 }
